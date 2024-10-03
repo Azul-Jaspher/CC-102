cc2fd434d2b5ffc1e4b147c7655179e29ba4270e
Azul-Jaspher <jaspherazul2006@gmail.com>
2024-10-03 10:18:09 +0800

    Add files via upload

---
+++ 04Basicinput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name,course,school;
+	cout <<"Nickname" << endl;
+	cin >> name;
+	cout <<"Course-Year Level" << endl;
+	cin >> course;
+	cout <<"School" << endl;
+	cin >>school;
+	
+	cout <<"Wow congrats" << name << "!" <<course << "is a nice a course. And you are studying in" << school <<"which is one of the Center of Execellence in Tertiary Education";
+	return 0;
+
+}
