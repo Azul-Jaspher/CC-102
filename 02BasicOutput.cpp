f907093b1712b7b463b9b8a9a86bc9f08e337bea
Azul-Jaspher <jaspherazul2006@gmail.com>
2024-10-03 09:48:13 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College Computer Studies" << endl;
+	cout << "BSIT 1-A" << endl;
+	cout << "Jaspher John Thadeus O, Azul" << endl;
+	cout << "Brgy 3 Pob.1 Vinzons Camarines Norte" << endl;
+	
+	return 0;
+
+}
