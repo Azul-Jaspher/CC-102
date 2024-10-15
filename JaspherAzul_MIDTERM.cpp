a4da6609cfe5f57e7f92163357e2fbe4b5434736
Azul-Jaspher <jaspherazul2006@gmail.com>
2024-10-15 10:12:53 +0800

    Add files via upload

---
+++ JaspherAzul_MIDTERM.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sugarPriceUSD,ricePriceUSD,sardinesPricePound,coffePriceUD,milkPriceUSD;
+	int sugarQty,riceQty,sardinesQty,coffeQty,milkQty;
+	int sugarcostinUSD,ricecostinPound,sardinescostinUSD,cofffecostinUSD,milkcostinUSD;
+	int usdToPhp,poundToPhp;
+	
+	cout << "sugar cost in USD" << endl;
+	cin >>sugarQty;
+	cout << "rice cost in Pound"<< endl;
+	cin >>riceQty;
+	cout << "sardines cost in USD" << endl;
+	cin >>sardinesQty;
+	cout << "coffe cost in USD" << endl;
+	cin >>coffeQty;
+	cout << "milk cost in USD" << endl;
+	cin >>milkQty;
+	
+	
+	sugarPriceUSD = sugarQty * sugarcost
+	return 0;
+}
