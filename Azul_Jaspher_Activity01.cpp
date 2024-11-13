e869b188ebe1f3bf19a6a7ed2fde9cd614d2b4fe
Azul-Jaspher <jaspherazul2006@gmail.com>
2024-11-13 08:54:56 +0800

    Add files via upload

---
+++ Azul_Jaspher_Activity01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int Temperature;
+	
+	cout << "What is The Temperature" <<endl;
+	cin >> Temperature;
+	if (Temperature < 32){
+		cout << "Bring a heavy jacket" <<endl;
+	}else if (Temperature <= 50){
+		cout << "Bring light jacket" <<endl;
+	}else {
+		cout << "Do not bring any jacket" <<endl;
+	}
+	return 0;
+}
